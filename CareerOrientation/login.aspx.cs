7f40d250ee60c3ed0305e7b63ffd09b526f5b1b8
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-05-13 17:56:39 +0300

    f

--- CareerOrientation/login.aspx.cs
+++ CareerOrientation/login.aspx.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Data.Sqlite;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Linq;
@@ -13,14 +12,14 @@ namespace CareerOrientation
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
         protected void Unnamed1_Click(object sender, EventArgs e)
         {
 
-            SQLiteConnection conn = new SqliteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
+            SQLiteConnection conn = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "career_base.db;Version=3;");
             conn.Open();
-            String query1 = "Select * from client where username='" + name1.Text + "' and pass='" + pwd.Text + "'";
+            String query1 = "Select * from users where username='" + name1.Text + "' and password='" + pwd.Text + "'";
             SQLiteCommand cmd = new SQLiteCommand(query1, conn);
             SQLiteDataReader reader = cmd.ExecuteReader();
             System.Text.StringBuilder builder = new System.Text.StringBuilder();
@@ -40,30 +39,7 @@ namespace CareerOrientation
                 Session["Username"] = name1.Text;
                 Session["Type"] = "Client";
                 Response.Redirect("clientMainPage.aspx");
-            }
-            SQLiteConnection conn2 = new SQLiteConnection("Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "hire_dev.client.db;Version=3;");
-            conn2.Open();
-            String query2 = "Select * from dev where username='" + name1.Text + "' and pass='" + pwd.Text + "'";
-            SQLiteCommand cmd2 = new SQLiteCommand(query2, conn2);
-            SQLiteDataReader reader2 = cmd2.ExecuteReader();
-            System.Text.StringBuilder builder2 = new System.Text.StringBuilder();
-            while (reader2.Read())
-            {
-                builder2.Append(reader2.GetString(1) + "/").Append(reader2.GetString(2));
-            }
-            conn2.Close();
-            if (builder2.ToString() == "")
-            {
-
-                string script = "alert(\"Incorrect username/password\")";
-                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
-            }
-            else
-            {
-                Session["Username"] = name1.Text;
-                Session["Type"] = "Developer";
-                Response.Redirect("devMainPage.aspx");
-            }
+            
 
         }
     }
